429ec84d969f30a722053b946db9f2d2d0eabb6d
Bran47 <93209532+Bran47@users.noreply.github.com>
2021-11-26 14:33:31 +0300

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double number1= 7.3;
+	double number2;
+	
+	double*myptr;
+	int number;
+	
+	number1 = 7.3;
+	cout<<"*ptr";
+	
+	return 0;
+}
